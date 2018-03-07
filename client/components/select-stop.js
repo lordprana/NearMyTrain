7f4329391c86d5e5930b075bbfd36c54bcb2f0f9
lordprana <matthew.gaba.2@gmail.com>
2018-03-07 15:55:43 -0500

    Updated seed script and added a station selector

--- client/components/select-stop.js
+++ client/components/select-stop.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import stops from '../data/subway-stops';
 
-const SelectStop = () => (
-  <div>
-    Hello world
-  </div>
+const getSortedStops = line => {
+  const filteredStops = stops.filter(stop => stop.hasOwnProperty(line));
+  return filteredStops.sort((a, b) => {
+    return a[line] - b[line];
+  });
+};
+
+const SelectStop = ({line}) => (
+  <select>
+    {
+      getSortedStops(line).map(stop => (
+        <option key={stop.OBJECTID} value={stop.OBJECTID}>{stop.NAME}</option>
+      ))
+    }
+  </select>
 );
 
 export default SelectStop;
--- script/transform-data.js
+++ script/transform-data.js
@@ -1,22 +1,41 @@
 const fs = require('fs');
 let subwayStops = require('./subway-stops-orig.json');
 
+// function distance(lat1, lon1, lat2, lon2) {
+// 	var radlat1 = Math.PI * lat1 / 180;
+// 	var radlat2 = Math.PI * lat2 / 180;
+// 	var theta = lon1 - lon2;
+// 	var radtheta = Math.PI * theta / 180;
+// 	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+// 	dist = Math.acos(dist);
+// 	dist = dist * 180 / Math.PI;
+// 	dist = dist * 60 * 1.1515;
+// 	return dist;
+// }
+
 function distance(lat1, lon1, lat2, lon2) {
-	var radlat1 = Math.PI * lat1 / 180;
-	var radlat2 = Math.PI * lat2 / 180;
-	var theta = lon1 - lon2;
-	var radtheta = Math.PI * theta / 180;
-	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-	dist = Math.acos(dist);
-	dist = dist * 180 / Math.PI;
-	dist = dist * 60 * 1.1515;
-	return dist;
+  var R = 6371; // Radius of the earth in km
+  var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+  var dLon = deg2rad(lon2 - lon1);
+  var a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+    Math.sin(dLon / 2) * Math.sin(dLon / 2)
+    ;
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  var d = R * c; // Distance in km
+  return d;
+}
+
+function deg2rad(deg) {
+  return deg * (Math.PI / 180);
 }
 
 function convertToLatLng(geom) {
-  const latRegex = /\((-[0-9]+\.[0-9]+) /;
-  const longRegex = / ([0-9]+\.[0-9]+)\)/;
-  return [+geom.match(latRegex)[1], +geom.match(longRegex)[1]];
+  // Latitude and longitude are reversed in the dataset
+  const lngRegex = /\((-[0-9]+\.[0-9]+) /;
+  const latRegex = / ([0-9]+\.[0-9]+)\)/;
+  return [+geom.match(latRegex)[1], +geom.match(lngRegex)[1]];
 }
 
 const lines = [
