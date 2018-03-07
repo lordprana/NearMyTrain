49136a94e534814e56f87c6069544571c33e3a31
lordprana <matthew.gaba.2@gmail.com>
2018-03-07 13:47:29 -0500

    Cleaned up boilermaker to only include functionality required by the app

--- client/app.js
+++ client/app.js
@@ -1,16 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-import {Navbar} from './components'
-import Routes from './routes'
+import {Home} from './components';
 
 
 const App = () => {
   return (
     <div>
-      <Navbar />
-      <Routes />
+      <Home />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- script/transform-data.js
+++ script/transform-data.js
@@ -1,20 +1,20 @@
 const fs = require('fs');
-let subwayStops = require('./subway-stops-orig.json')
+let subwayStops = require('./subway-stops-orig.json');
 
 function distance(lat1, lon1, lat2, lon2) {
-	var radlat1 = Math.PI * lat1/180
-	var radlat2 = Math.PI * lat2/180
-	var theta = lon1-lon2
-	var radtheta = Math.PI * theta/180
+	var radlat1 = Math.PI * lat1 / 180;
+	var radlat2 = Math.PI * lat2 / 180;
+	var theta = lon1 - lon2;
+	var radtheta = Math.PI * theta / 180;
 	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-	dist = Math.acos(dist)
-	dist = dist * 180/Math.PI
-	dist = dist * 60 * 1.1515
-	return dist
+	dist = Math.acos(dist);
+	dist = dist * 180 / Math.PI;
+	dist = dist * 60 * 1.1515;
+	return dist;
 }
 
 function convertToLatLng(geom) {
-  const latRegex = /\((\-[0-9]+\.[0-9]+) /;
+  const latRegex = /\((-[0-9]+\.[0-9]+) /;
   const longRegex = / ([0-9]+\.[0-9]+)\)/;
   return [+geom.match(latRegex)[1], +geom.match(longRegex)[1]];
 }
@@ -32,26 +32,26 @@ const lines = [
     line: 'E',
     terminalStop: 'Jamaica Ctr - Parsons / Archer'
   }
-]
+];
 
 lines.forEach(line => {
   let stopsOnLine = subwayStops.filter(stop => {
     // For 'E' line, make sure not to match 'Express' in LINE information
     if (line.line === 'E') {
-      if(stop.LINE.includes(line.line) && !stop.LINE.includes('Express')) {
-        return true
+      if (stop.LINE.includes(line.line) && !stop.LINE.includes('Express')) {
+        return true;
       }
-    } else {
-      if (stop.LINE.includes(line.line)) return true;
+    } else if (stop.LINE.includes(line.line)) {
+      return true;
     }
     return false;
-  })
+  });
 
   const terminalStop = stopsOnLine.find(stop => {
     if (stop.NAME === line.terminalStop && stop.LINE.includes(line.line)) {
       return true;
     }
-  })
+  });
 
   stopsOnLine = stopsOnLine.map(stop => {
     let [termLat, termLng] = convertToLatLng(terminalStop.the_geom);
@@ -60,11 +60,11 @@ lines.forEach(line => {
     stop.lng = stopLng;
     stop.distance = distance(termLat, termLng, stopLat, stopLng);
     return stop;
-  })
+  });
 
   let sortedStopsOnLine = stopsOnLine.sort((a, b) => {
     return a.distance - b.distance;
-  })
+  });
 
   sortedStopsOnLine.forEach((stop, i) => {
     subwayStops = subwayStops.map(s => {
@@ -73,12 +73,9 @@ lines.forEach(line => {
       }
       delete s.distance;
       return s;
-    })
-  })
-})
+    });
+  });
+});
 
-fs.writeFile('subway-stops-transformed.json', JSON.stringify(subwayStops))
-
-
-
+fs.writeFile('subway-stops-transformed.json', JSON.stringify(subwayStops));
 
